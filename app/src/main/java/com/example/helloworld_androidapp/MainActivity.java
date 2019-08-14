0f4c87cc90cd3b97fe756dabea399c495cf9285c
Tomoko117 <cgub0037@mail4.doshisha.ac.jp>
2019-08-14 15:31:44 +0900

    レイアウトを動的に変化させることを可能にした．

--- app/src/main/java/com/example/helloworld_androidapp/MainActivity.java
+++ app/src/main/java/com/example/helloworld_androidapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.helloworld_androidapp;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button; //ボタン
+import android.widget.LinearLayout;
 import android.widget.TextView; //テキスト
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +18,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //activity_main.xmlファイルをレイアウトとして登録
+        //レイアウトファイルは呼ばない　アプリ起動中に変更できないため
+        //setContentView(R.layout.activity_main); //activity_main.xmlファイルをレイアウトとして登録 静的
+
+        //リニアレイアウトの設定
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL); //子ビューを追加する方向を垂直方向にする
+        //横幅・縦幅の指定 画面いっぱい
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+
+        layout.setGravity(Gravity.CENTER); // レイアウト中央寄せ
+        setContentView(layout);
+
 
         //ボタン設定
-        Button button = findViewById(R.id.button); //レイアウトファイルからidがbuttonのものを見つけてくる
+        //Button button = findViewById(R.id.button); //レイアウトファイルからidがbuttonのものを見つけてくる
+        Button button = new Button(this);
+        button.setText(R.string.push);
+        float scale = getResources().getDisplayMetrics().density; // dp単位を取得 画面解像度の比率
+        int buttonWidth = (int)(150 * scale);
+        //横幅・縦幅の指定　buttonWidth・必要なサイズ
+        LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
+                buttonWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
+        // マージン設定
+        int margins = (int)(50 * scale);
+        buttonLayoutParams.setMargins(margins, margins, margins, margins);
+
+        button.setLayoutParams(buttonLayoutParams);
+        layout.addView(button);
+
         //テキスト設定
-        textView = findViewById(R.id.textView);
+        //textView = findViewById(R.id.textView);
+        textView = new TextView(this);
+        textView.setText(R.string.hello);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30); // テキストサイズ 30sp
+        textView.setTextColor(Color.rgb(0x0, 0x0, 0x0)); // テキストカラー
+        //横幅・縦幅の指定　必要なサイズ
+        LinearLayout.LayoutParams textLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(textLayoutParams);
+        layout.addView(textView);
 
         //ボタンが押された時の処理
         button.setOnClickListener(new View.OnClickListener(){
